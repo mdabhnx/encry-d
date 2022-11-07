f2b6f7ff90305d0301bf49805c9bdae24967aae8
mdabhnx <mdabhnx+github@gmail.com>
2022-11-07 11:32:41 +0600

    loading state added.

--- encryd-client/src/App.jsx
+++ encryd-client/src/App.jsx
@@ -1,23 +1,31 @@
-import React, { useState } from 'react'
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import { CopyToClipboard } from 'react-copy-to-clipboard'
-import CopyIcon from './assets/copy.png'
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import CopyIcon from "./assets/copy.png";
+
+const Loading = () => {
+  return <>Loading...</>;
+};
 
 const App = () => {
-  const [content, setContent] = useState('')
-  const [masterKey, setMasterKey] = useState('')
-  const [btnDisabledStatus, setBtnDisabledStatus] = useState(false)
-  const [contentOutput, setContentOutput] = useState('')
+  const [content, setContent] = useState("");
+  const [masterKey, setMasterKey] = useState("");
+  const [btnDisabledStatus, setBtnDisabledStatus] = useState(false);
+  const [contentOutput, setContentOutput] = useState("");
 
   const contentEncryptionHandler = () => {
-    setContentOutput('')
-    setBtnDisabledStatus(true)
+    if (content.trim().length === 0 || masterKey.trim().length === 0) {
+      alert("filed is empty!");
+      return;
+    }
+    setContentOutput("");
+    setBtnDisabledStatus(true);
 
     fetch(`${process.env.REACT_APP_BASE_API_URL}/v1/encryd/encr`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: masterKey,
       },
       body: JSON.stringify({
@@ -26,28 +34,33 @@ const App = () => {
     })
       .then((res) => res.json())
       .then((res) => {
-        setMasterKey('')
-        setBtnDisabledStatus(false)
+        setMasterKey("");
+        setBtnDisabledStatus(false);
         if (res.status === 200) {
-          setContentOutput(res.data.content)
-          toast.success('Encrypt Complete!')
+          setContentOutput(res.data.content);
+          toast.success("Encrypt Complete!");
         }
       })
       .catch((err) => {
-        setMasterKey('')
-        setBtnDisabledStatus(false)
-        console.log(err)
-      })
-  }
+        setMasterKey("");
+        setBtnDisabledStatus(false);
+        console.log(err);
+      });
+  };
 
   const contentDecryptionHandler = () => {
-    setContentOutput('')
-    setBtnDisabledStatus(true)
+    if (content.trim().length === 0 || masterKey.trim().length === 0) {
+      alert("filed is empty!");
+      return;
+    }
+
+    setContentOutput("");
+    setBtnDisabledStatus(true);
 
     fetch(`${process.env.REACT_APP_BASE_API_URL}/v1/encryd/decr`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: masterKey,
       },
       body: JSON.stringify({
@@ -56,33 +69,33 @@ const App = () => {
     })
       .then((res) => res.json())
       .then((res) => {
-        setMasterKey('')
-        setBtnDisabledStatus(false)
+        setMasterKey("");
+        setBtnDisabledStatus(false);
         if (res.status === 200) {
-          setContentOutput(res.data.content)
-          toast.success('Decrypt Complete!')
+          setContentOutput(res.data.content);
+          toast.success("Decrypt Complete!");
         }
       })
       .catch((err) => {
-        setMasterKey('')
-        setBtnDisabledStatus(false)
-        console.log(err)
-      })
-  }
+        setMasterKey("");
+        setBtnDisabledStatus(false);
+        console.log(err);
+      });
+  };
 
   return (
     <div
-      className='main_container'
+      className="main_container"
       style={{
-        display: 'flex',
-        padding: '10px',
-        alignItems: 'center',
-        flexDirection: 'column',
-        width: '100vw',
+        display: "flex",
+        padding: "10px",
+        alignItems: "center",
+        flexDirection: "column",
+        width: "100vw",
       }}
     >
       <ToastContainer
-        position='top-center'
+        position="top-center"
         autoClose={1000}
         hideProgressBar={true}
         closeOnClick={true}
@@ -94,78 +107,78 @@ const App = () => {
       <span hidden>Welcome Master Hossain</span>
 
       <div
-        className='input__container'
+        className="input__container"
         style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '40vh',
-          width: '100vw',
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "40vh",
+          width: "100vw",
         }}
       >
         <input
-          type='text'
+          type="text"
           style={{
-            height: '52px',
-            width: '40%',
-            padding: '8px',
-            fontSize: '18px',
-            borderRadius: '6px',
+            height: "52px",
+            width: "40%",
+            padding: "8px",
+            fontSize: "18px",
+            borderRadius: "6px",
           }}
           value={content}
           onChange={(e) => setContent(e.target.value)}
-          placeholder='Enter Your Content Here...'
+          placeholder="Enter Your Content Here..."
         />
 
         <input
-          type='password'
+          type="password"
           style={{
-            height: '52px',
-            width: '30%',
-            padding: '8px',
-            fontSize: '18px',
-            borderRadius: '6px',
-            marginTop: '10px',
+            height: "52px",
+            width: "30%",
+            padding: "8px",
+            fontSize: "18px",
+            borderRadius: "6px",
+            marginTop: "10px",
           }}
           value={masterKey}
           onChange={(e) => setMasterKey(e.target.value)}
-          placeholder='Your Master Key...'
+          placeholder="Your Master Key..."
         />
 
         <div
-          className='action__container'
-          style={{ marginTop: '10px', fontSize: '14px' }}
+          className="action__container"
+          style={{ marginTop: "10px", fontSize: "14px" }}
         >
           <button
             style={{
-              fontSize: '14px',
-              padding: '10px',
-              minWidth: '92px',
-              borderRadius: '4px',
-              backgroundColor: 'green',
-              color: 'wheat',
+              fontSize: "14px",
+              padding: "10px",
+              minWidth: "92px",
+              borderRadius: "4px",
+              backgroundColor: "green",
+              color: "wheat",
             }}
             onClick={() => contentEncryptionHandler()}
             disabled={btnDisabledStatus}
           >
-            Encrypt
+            {btnDisabledStatus ? <Loading /> : "Encrypt"}
           </button>
 
           <button
             style={{
-              marginLeft: '10px',
-              fontSize: '14px',
-              padding: '10px',
-              minWidth: '92px',
-              borderRadius: '4px',
-              backgroundColor: 'yellow',
-              color: 'black',
+              marginLeft: "10px",
+              fontSize: "14px",
+              padding: "10px",
+              minWidth: "92px",
+              borderRadius: "4px",
+              backgroundColor: "yellow",
+              color: "black",
             }}
             onClick={() => contentDecryptionHandler()}
             disabled={btnDisabledStatus}
           >
-            Decrypt
+            {btnDisabledStatus ? <Loading /> : "Decrypt"}
           </button>
         </div>
       </div>
@@ -173,31 +186,31 @@ const App = () => {
       {contentOutput && (
         <CopyToClipboard text={contentOutput}>
           <div
-            className='output__container'
+            className="output__container"
             style={{
-              marginTop: '20px',
-              padding: '10px',
-              borderRadius: '4px',
-              width: '50%',
-              display: 'flex',
-              justifyContent: 'space-between',
+              marginTop: "20px",
+              padding: "10px",
+              borderRadius: "4px",
+              width: "50%",
+              display: "flex",
+              justifyContent: "space-between",
             }}
           >
-            <p style={{ overflow: 'hidden' }}>{contentOutput}</p>
+            <p style={{ overflow: "hidden" }}>{contentOutput}</p>
             <p>
               <img
-                className='copy__icon'
+                className="copy__icon"
                 src={CopyIcon}
-                alt='copy-icon'
-                style={{ height: '20px', width: '20px' }}
-                title='Click to copy!!!'
+                alt="copy-icon"
+                style={{ height: "20px", width: "20px" }}
+                title="Click to copy!!!"
               />
             </p>
           </div>
         </CopyToClipboard>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
